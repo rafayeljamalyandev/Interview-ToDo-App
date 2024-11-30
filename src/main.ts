6ab1b23c06e38802c3efdfbde9545dc89429850c
onurkantarci <onurkntrc@gmail.com>
2024-11-30 15:49:40 +0300

    feat(app): enable CORS and shutdown hooks

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,14 @@ import { AppModule } from './application/app/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors();
+
   await app.listen(3000, () => {
     console.log('Server started listening on port 3000');
   });
+
+  app.enableShutdownHooks();
 }
+
 bootstrap();
