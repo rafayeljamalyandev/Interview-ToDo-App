fb2bab1c64b03389bd2a7c463a8f24579f782792
Mahmoud-Mehri <mahmoud.mehri90@gmail.com>
2024-12-01 19:39:54 +0000

    refactor: update Todo Service to: 1. handle exceptions 2. use DTO as input 3. return customized response 4. fix awaiting for promisses

--- src/todo/todos.service.ts
+++ src/todo/todos.service.ts
@@ -1,17 +1,38 @@
 import { Injectable } from '@nestjs/common';
-import { PrismaService } from './prisma.service';
+import { PrismaService } from '../prisma/prisma.service';
+import { CreateTodoDto } from './dtos/todo.dto';
+import {
+  errorResponse,
+  ServiceResponse,
+  successResponse,
+} from 'src/shared/utils';
 
 @Injectable()
 export class TodosService {
   constructor(private prisma: PrismaService) {}
 
-  async createTodo(userId: number, title: string) {
-    return this.prisma.todo.create({
-      data: { title, userId },
-    });
+  async createTodo(
+    userId: number,
+    TodoInfo: CreateTodoDto,
+  ): Promise<ServiceResponse> {
+    try {
+      const todo = await this.prisma.todo.create({
+        data: { userId, title: TodoInfo.title },
+      });
+
+      return successResponse(todo);
+    } catch (err) {
+      return errorResponse(); // Will return Internal Server Error by default
+    }
   }
 
   async listTodos(userId: number) {
-    return this.prisma.todo.findMany({ where: { userId } });
+    try {
+      const todoList = await this.prisma.todo.findMany({ where: { userId } });
+
+      return successResponse(todoList);
+    } catch (err) {
+      return errorResponse(); // Will return Internal Server Error by default
+    }
   }
 }
