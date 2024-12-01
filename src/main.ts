94e36d60b405200d8c11e6aed561026b61452ad4
Mahmoud-Mehri <mahmoud.mehri90@gmail.com>
2024-12-01 20:01:45 +0000

    Main file updated to 1. use validation pipe 2. use global exception filter 3. use dynamic port number 4. better handling app initialization status

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { AllExceptionFilter } from './shared/middlewares/exception.filter';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000, () => {
-    console.log('Server started listening on port 3000');
-  });
+  app.use(new ValidationPipe({ transform: true, whitelist: true }));
+  app.use(new AllExceptionFilter());
+  const configService = app.get<ConfigService>(ConfigService);
+  const port = configService.get<number>('PORT') || 3000;
+  app
+    .listen(port)
+    .then(() => {
+      console.log(`Server started listening on port ${port}`);
+    })
+    .catch((err) => {
+      console.log(`Error on running server on port ${port}`, err);
+    });
 }
 bootstrap();
