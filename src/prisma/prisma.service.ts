54007b7a540180aa205708d25441d89c81d3a215
Mahmoud-Mehri <mahmoud.mehri90@gmail.com>
2024-12-01 21:38:53 +0000

    Update Prisma service to implement Module init and destroy for connecting and disconnecting

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,17 +1,28 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { PrismaClient } from '@prisma/client';
 
 @Injectable()
-export class PrismaService extends PrismaClient {
-  constructor(private configService: ConfigService) {
+export class PrismaService
+  extends PrismaClient
+  implements OnModuleInit, OnModuleDestroy
+{
+  constructor(private readonly configService: ConfigService) {
     super({
       datasources: {
         db: {
           url: configService.get<string>('DATABASE_URL'),
         },
       },
-      log: ['info'],
+      log: ['error'],
     });
+  }
+
+  async onModuleInit() {
+    await this.$connect();
+  }
+
+  async onModuleDestroy() {
+    await this.$disconnect();
   }
 }
