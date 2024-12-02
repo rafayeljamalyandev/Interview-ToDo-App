00c06b1049ef7fc0c9efcf79193d0b75f6203925
Mahmoud-Mehri <mahmoud.mehri90@gmail.com>
2024-12-02 16:47:39 +0000

    Add options parameter into constructor

--- src/shared/middlewares/validation.pipe.ts
+++ src/shared/middlewares/validation.pipe.ts
@@ -1,8 +1,13 @@
-import { BadRequestException, ValidationPipe } from '@nestjs/common';
+import {
+  BadRequestException,
+  ValidationPipe,
+  ValidationPipeOptions,
+} from '@nestjs/common';
 
 export class CustomValidationPipe extends ValidationPipe {
-  constructor() {
+  constructor(options?: ValidationPipeOptions) {
     super({
+      ...options,
       exceptionFactory: (errors) => {
         const customErrors = errors.map((error) => {
           return `${error.property} - ${Object.values(error.constraints).join(', ')}`;
