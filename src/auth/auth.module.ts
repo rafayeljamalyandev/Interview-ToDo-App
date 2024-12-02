b5b05ecaeefaf011dd94a7fc5b257e4a99436d24
Mahmoud-Mehri <mahmoud.mehri90@gmail.com>
2024-12-02 15:26:39 +0000

    Add User Repository to Auth module as provider

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,10 +2,14 @@ import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { PrismaModule } from '../prisma/prisma.module';
+import { UserRepository } from './repositories/user.repository';
 
 @Module({
   imports: [PrismaModule],
-  providers: [AuthService],
+  providers: [
+    AuthService,
+    { provide: 'IUserRepository', useClass: UserRepository },
+  ],
   controllers: [AuthController],
 })
 export class AuthModule {}
