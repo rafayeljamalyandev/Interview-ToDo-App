66be9963700be18227c1aec9a0acf4637442bf88
Mahmoud-Mehri <mahmoud.mehri90@gmail.com>
2024-12-02 16:48:32 +0000

    changed to use CustomValidationPipe

--- src/main.ts
+++ src/main.ts
@@ -3,14 +3,17 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { AllExceptionFilter } from './shared/middlewares/exception.filter';
 import { ConfigService } from '@nestjs/config';
+import { CustomValidationPipe } from './shared/middlewares/validation.pipe';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe({ transform: true, whitelist: true }));
+  app.useGlobalPipes(
+    new CustomValidationPipe({ transform: true, whitelist: true }),
+  );
   app.useGlobalFilters(new AllExceptionFilter());
   const configService = app.get<ConfigService>(ConfigService);
   const port = configService.get<number>('PORT') || 3000;
-  app
+  await app
     .listen(port)
     .then(() => {
       console.log(`Server started listening on port ${port}`);
