ebdbe41d72899bf22b94040519d96720fe090893
Mahmoud-Mehri <mahmoud.mehri90@gmail.com>
2024-12-02 14:53:54 +0000

    Add comment to clear the reason for using string type for Id

--- src/auth/models/model.ts
+++ src/auth/models/model.ts
@@ -1,4 +1,6 @@
 export class User {
+  // Using string type for Id will make us able to change event Database
+  // without making any changes on service layer
   constructor(
     public id: string,
     public email: string,
