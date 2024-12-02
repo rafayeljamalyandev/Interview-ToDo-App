4a67ceb9145be6788cc78f86eb77451f0b6810b3
Mahmoud-Mehri <mahmoud.mehri90@gmail.com>
2024-12-02 10:11:30 +0000

    refactor: Migration SQL code updated to 1. use proper length for VARCHAR for better indexing 2. use Foreign Key constraint for userId 3. use set Engine to InnoDB

--- prisma/migrations/20241126144623_initial/migration.sql
+++ prisma/migrations/20241126144623_initial/migration.sql
@@ -1,19 +1,22 @@
 -- CreateTable
 CREATE TABLE `User` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
-    `email` VARCHAR(191) NOT NULL,
-    `password` VARCHAR(191) NOT NULL,
+    `email` VARCHAR(100) NOT NULL,
+    `password` VARCHAR(60) NOT NULL,
 
     UNIQUE INDEX `User_email_key`(`email`),
     PRIMARY KEY (`id`)
-) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+) ENGINE=InnoDB DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- CreateTable
 CREATE TABLE `Todo` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
-    `title` VARCHAR(191) NOT NULL,
+    `title` VARCHAR(100) NOT NULL,
     `completed` BOOLEAN NOT NULL DEFAULT false,
     `userId` INTEGER NOT NULL,
 
     PRIMARY KEY (`id`)
-) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+    FOREIGN KEY (`userId`) REFERENCES `User`(`id`) 
+        ON DELETE CASCADE
+        ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
