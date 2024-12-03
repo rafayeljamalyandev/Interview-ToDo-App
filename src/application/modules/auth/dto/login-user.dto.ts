82a3fb25e28d269fe7bb087c117e9c5379117576
onurkantarci <onurkntrc@gmail.com>
2024-12-03 19:38:39 +0300

    feat(dto): enhance LoginUserDto validation with email format and non-empty checks

--- src/application/modules/auth/dto/login-user.dto.ts
+++ src/application/modules/auth/dto/login-user.dto.ts
@@ -1,11 +1,14 @@
-import { IsString, Length } from 'class-validator';
+import { IsEmail, IsNotEmpty, IsString, Length } from 'class-validator';
 
 export class LoginUserDto {
   @IsString()
+  @IsNotEmpty()
+  @IsEmail()
   @Length(5, 25)
   email: string;
 
   @IsString()
+  @IsNotEmpty()
   @Length(6, 12)
   password: string;
 }
