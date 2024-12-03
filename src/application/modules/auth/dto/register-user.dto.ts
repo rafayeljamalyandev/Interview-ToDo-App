ccc19449ad121e5acd02d5af12af292b1f8a0865
onurkantarci <onurkntrc@gmail.com>
2024-12-03 19:39:01 +0300

    feat(dto): enhance RegisterUserDto validation with email format and non-empty checks

--- src/application/modules/auth/dto/register-user.dto.ts
+++ src/application/modules/auth/dto/register-user.dto.ts
@@ -1,11 +1,14 @@
-import { IsString, Length } from 'class-validator';
+import { IsEmail, IsNotEmpty, IsString, Length } from 'class-validator';
 
 export class RegisterUserDto {
   @IsString()
+  @IsEmail()
+  @IsNotEmpty()
   @Length(5, 25)
   email: string;
 
   @IsString()
+  @IsNotEmpty()
   @Length(6, 12)
   password: string;
 }
