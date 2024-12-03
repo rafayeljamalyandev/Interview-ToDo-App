c07fb64d4060585abec5f2381259397419f99228
Mahmoud-Mehri <mahmoud.mehri90@gmail.com>
2024-12-03 15:11:13 +0000

    Import AuthModule to use UserRepository

--- src/todo/todos.module.ts
+++ src/todo/todos.module.ts
@@ -2,8 +2,10 @@ import { Module } from '@nestjs/common';
 import { TodoService } from './todos.service';
 import { TodosController } from './todos.controller';
 import { TodoRepository } from './repositories/todo.repository';
+import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
+  imports: [AuthModule],
   providers: [
     TodoService,
     { provide: 'ITodoRepository', useClass: TodoRepository },
