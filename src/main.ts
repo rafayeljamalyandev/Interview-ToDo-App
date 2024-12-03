621deef376d58c18fbc89ab536d538d966cc5e3e
Nancy Asiuloka <nancyasiuloka@gmail.com>
2024-12-03 22:15:22 +0100

    Feat: Extracted the port into a variable for easier updates, and template literals improved the log message's clarity and flexibility.

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
+  const logger = new Logger('Bootstrap');
+
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000, () => {
-    console.log('Server started listening on port 3000');
+  const port = process.env.PORT;
+
+  await app.listen(port, () => {
+    logger.log(`Server started listening on port ${port}`);
   });
 }
 bootstrap();
