a8a7a81357eb286a045c7ea6f0578416b904cd02
onurkantarci <onurkntrc@gmail.com>
2024-12-03 19:43:17 +0300

    chore(todos): add PrismaModule, JwtModule, and strategies to TodosModule

--- src/application/modules/todos/todos.module.ts
+++ src/application/modules/todos/todos.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { TodosService } from './todos.service';
 import { TodosController } from './todos.controller';
+import { JwtModule } from '@nestjs/jwt';
+import { PrismaModule } from '../../../infrastructure/database/prisma.module';
+import { AtStrategy, RtStrategy } from '../auth/strategies';
 
 @Module({
-  providers: [TodosService],
+  imports: [PrismaModule, JwtModule.register({})],
+  providers: [TodosService, AtStrategy, RtStrategy],
   controllers: [TodosController],
 })
 export class TodosModule {}
